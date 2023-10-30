939b7197387b7325239383010b1789ed280ba885
Daniel <danielrs9504@gmail.com>
2023-10-30 17:28:57 -0500

    Data Encapsulation: update create_post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,16 +22,9 @@ class ApplicationController < ActionController::Base
   end
 
   def create_post
-    insert_query = <<-SQL
-      INSERT INTO posts (title, body, author, created_at)
-      VALUES (?, ?, ?, ?)
-    SQL
+    post = Post.new('title' => params['title'], 'body' => params['body'], 'author' => params['author'])
 
-    connection.execute insert_query,
-                       params['title'],
-                       params['body'],
-                       params['author'],
-                       Date.current.to_s
+    post.save
 
     redirect_to '/list_posts'
   end

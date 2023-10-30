8eed1d64b14618e68a1f071a32be345626a82d62
Daniel <danielrs9504@gmail.com>
2023-10-30 17:26:09 -0500

    Data Encapsulation: move create_post to Post class

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,4 +8,23 @@ class Post
     @author = attributes['author']
     @created_at = attributes['created_at']
   end
+
+  def save
+    insert_query = <<-SQL
+      INSERT INTO posts (title, body, author, created_at)
+      VALUES (?, ?, ?, ?)
+    SQL
+
+    connection.execute insert_query,
+                       title,
+                       body,
+                       author,
+                       Date.current.to_s
+  end
+
+  def connection
+    db_connection = SQLite3::Database.new('db/development.sqlite3')
+    db_connection.result_as_hash = true
+    db_connection
+  end
 end

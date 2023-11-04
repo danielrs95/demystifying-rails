bac9a4e7728aca4767ff2d859888bd1d69019c4e
Daniel <danielrs9504@gmail.com>
2023-11-04 15:36:21 -0500

    Routes and Resources: modify routes using rails conventions/notation

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,24 @@
 Rails.application.routes.draw do
   get '/hello_world' => 'application#hello_world'
-  get '/list_posts' => 'application#list_posts'
-  get '/show_post/:id' => 'application#show_post'
-  get '/new_post' => 'application#new_post'
-  post '/create_post' => 'application#create_post'
 
-  get '/edit_post/:id' => 'application#edit_post'
-  post '/update_post/:id' => 'application#update_post'
-  post '/delete_post/:id' => 'application#delete_post'
+  ### posts ###
 
-  post '/create_comment_for_post/:post_id' => 'application#create_comment'
-  post '/list_posts/:post_id/delete_comment/:comment_id' => 'application#delete_comment'
+  # get    '/posts'          => 'posts#index'
+  # get    '/posts/new'      => 'posts#new'
+  # get    '/posts/:id'      => 'posts#show'
+  # post   '/posts'          => 'posts#create'
+  # get    '/posts/:id/edit' => 'posts#edit'
+  # patch  '/posts/:id'      => 'posts#update'
+  # put    '/posts/:id'      => 'posts#update'
+  # delete '/posts/:id'      => 'posts#destroy'
 
-  get '/list_comments' => 'application#list_comments'
+  ### comments ###
+
+  # get    '/comments' => 'comments#index'
+  # post   '/posts/:id/comments' => 'comments#create'
+  # delete '/posts/:post_id/comments/:id' => 'comments#destroy'
+
+  resources :posts do
+    resources :comments, only: %i[create destroy]
+  end
 end

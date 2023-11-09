02a7ea00a5b426ffbbb1a52b9a9ec389120fcbb7
Daniel <danielrs9504@gmail.com>
2023-11-09 07:23:45 -0500

    Conventional Views and Controller Actions: Add instance variables and modifies show_post view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,8 @@
 class PostsController < ApplicationController
   def show # <- used to be: shown_post
-    post = Post.find(params['id'])
-    comment = Comment.new
+    @post = Post.find(params['id'])
+    @comment = Comment.new
 
-    render 'application/show_post', locals: { post: post, comment: comment }
+    render 'application/show_post'
   end
 end

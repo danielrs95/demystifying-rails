e7f36b2d94e7fc48547f1901a754890c16b75987
Daniel <danielrs9504@gmail.com>
2023-11-09 07:34:50 -0500

    Views & Controllers: Move show_post to follow rails convention

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,5 @@ class PostsController < ApplicationController
   def show # <- used to be: shown_post
     @post = Post.find(params['id'])
     @comment = Comment.new
-
-    render 'application/show_post'
   end
 end

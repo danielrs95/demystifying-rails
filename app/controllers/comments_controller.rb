c46bd4d87f916f0ddcb0d5fd3cf8cba8cea6b671
Daniel <danielrs9504@gmail.com>
2023-11-11 05:23:49 -0500

    Views & Controllers: Add comments_controller and fix views

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,29 @@
+### app/controllers/comments_controller.rb ###
+
+class CommentsController < ApplicationController
+  # def list_comments -> list -> index
+  def index
+    @comments = Comment.all
+  end
+
+  # def create_comment -> create
+  def create
+    @post    = Post.find params['post_id']
+    @comment = @post.build_comment('author' => params['author'],
+                                   'body' => params['body'])
+
+    if @comment.save
+      redirect_to post_path(@post.id)
+    else
+      render 'posts/show'
+    end
+  end
+
+  # def delete_comment -> delete -> destroy
+  def destroy
+    post = Post.find(params['post_id'])
+    post.delete_comment(params['id'])
+
+    redirect_to post_path(post.id)
+  end
+end

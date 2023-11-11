97a1b027a80a24735bda55bd2cf3163d39d4e06b
Daniel <danielrs9504@gmail.com>
2023-11-11 15:56:23 -0500

    Models: Models inherit from ActiveRecord::Base

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,35 +1,6 @@
-class Comment < BaseModel
-  attr_reader :id, :body, :author, :post_id, :created_at, :errors
+### app/models/comment.rb ###
 
-  def initialize(attributes = {})
-    @id = attributes['id'] if new_record?
-    @body = attributes['body']
-    @author = attributes['author']
-    @post_id = attributes['post_id']
-    @created_at ||= attributes['created_at']
-    @errors = {}
-  end
-
-  def valid?
-    @errors['body'] = "Can't be blank" if body.blank?
-    @errors['author'] = "Can't be blank" if author.blank?
-    @errors.empty?
-  end
-
-  def post
-    Post.find(post_id)
-  end
-
-  def insert
-    insert_comment_query = <<-SQL
-      INSERT INTO comments (body, author, post_id, created_at)
-      VALUES (?, ?, ?, ?)
-    SQL
-
-    connection.execute insert_comment_query,
-                       @body,
-                       @author,
-                       @post_id,
-                       Date.current.to_s
-  end
+class Comment < ActiveRecord::Base
+  validates_presence_of :body, :author
 end
+

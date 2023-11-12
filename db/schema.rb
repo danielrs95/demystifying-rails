de76160a98ef50a977faf413b246d59d34eade2f
Daniel <danielrs9504@gmail.com>
2023-11-12 10:06:18 -0500

    Migrations: Run migration to add last_commented_on

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2015_01_27_115017) do
+ActiveRecord::Schema.define(version: 2023_11_12_150505) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2015_01_27_115017) do
     t.string "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "last_commented_on"
   end
 
 end
